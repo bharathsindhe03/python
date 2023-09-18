f8cd61c95e370a439b70e5161cbc358c44ee72d5
Bharath Sindhe <117362068+bharathsindhe03@users.noreply.github.com>
2023-09-18 11:46:37 +0530

    Add files via upload

---
+++ prg 3a.py
@@ -0,0 +1,13 @@
+word = input("Enter a word : ")
+digi = ucase = lcase = wordcnt = 0
+wordcont=word.split()
+for x in word:
+    if x>='0' and x<='9':
+        digi += 1
+    if x>='A'and x<='Z':
+        ucase += 1
+    if x>='a' and x<='z':
+        lcase += 1
+print("This word has\n")
+print("words: ", len(wordcont),"\n" "digits",digi, )
+print("upper case letters",ucase, "\n" "lower case letters ",lcase)
