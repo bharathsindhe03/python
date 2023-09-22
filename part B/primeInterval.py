5769a9b8986a9f35c87d86d490f50870bc77f0fd
Bharath Sindhe <117362068+bharathsindhe03@users.noreply.github.com>
2023-09-22 16:45:57 +0530

    Delete part B directory

--- part B/primeInterval.py
+++
@@ -1,9 +0,0 @@
-lower=int(input("entre upper limt"))
-upper=int(input("enter lower limit"))
-for num in range(lower, upper + 1):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-               break
-        else:
-            print(num)
