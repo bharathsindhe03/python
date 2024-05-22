839928af595a8796e342dc6bbba437d484f22deb
Bharath Sindhe <117362068+bharathsindhe03@users.noreply.github.com>
2024-05-22 16:32:53 +0530

    Update and rename prg 2a.py to Program_2/prg 2a.py

--- Program_2/prg 2a.py
+++ Program_2/prg 2a.py
@@ -1,3 +1,4 @@
+# Defined as a function F as Fn = Fn-1 + Fn-2. Write a Python program which accepts a value for N (where N >0) as input and pass this value to the function. Display suitable error message if the condition for input value is not followed. 
 def Fibonacci(n):
     if n == 1:
         return 0
