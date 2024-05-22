20b46b528bb9b46d21333a7d46c948e50dbeb1dd
Bharath Sindhe <117362068+bharathsindhe03@users.noreply.github.com>
2024-05-22 16:36:11 +0530

    Update and rename prg 5a.py to Program_5/prg 5a.py

--- Program_5/prg 5a.py
+++ Program_5/prg 5a.py
@@ -1,3 +1,4 @@
+# Write a function called isphonenumber () to recognize a pattern 415-555-4242 without using regular expression and also write the code to recognize the same pattern using regular expression. 
 import re
 def isphoneNo(num): 
     if(len(num)==12 and num[0:3].isdigit() and num[4:7].isdigit() and num[9:12].isdigit() and num[3]=='-'and num[7]=='-'):
