abe94b84aa8f5bc6bcab6e8be54d7d146ceacaff
Bharath Sindhe <117362068+bharathsindhe03@users.noreply.github.com>
2024-05-22 16:35:28 +0530

    Update and rename prg 4b.py to Program_4/prg 4b.py

--- Program_4/prg 4b.py
+++ Program_4/prg 4b.py
@@ -1,3 +1,4 @@
+# Write a program to convert roman numbers in to integer values using dictionaries. 
 def roman2integer(roman_numeral): 
     roman_dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000} 
     res=0 
